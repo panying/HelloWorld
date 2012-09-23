744a64a843606e7203410636d8f181c06ab92c3d
Ying <candypan515@gmail.com>
2012-09-22 22:38:52 -0400

    Initial commit

---
+++ HelloWorld/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '28f02e58320f23f8484cf9dda9b8db84f3be72c59836f3d2250be452015fc4669b444227a6d5fc766210d0fb2e6d21b8b547b2249085ae4c035b287ddb5be188'
---
+++ plusdollar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plusdollar::Application.config.secret_token = '0e20a6965677cdb16ce26dcb67223480ddb0b9001d03c43ffc03585629a25dc2d38065d8af8d94ce17cc5419b02c1faab7d8ee9f031ca7485a0b4c551884514c'
